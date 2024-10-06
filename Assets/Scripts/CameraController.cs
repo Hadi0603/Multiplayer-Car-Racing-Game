5673a5a5248cbc8886221a295fdd25dd904608b1
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-10-06 23:16:45 +0500

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,9 @@ public class CameraController : MonoBehaviour
     [SerializeField] GameObject child;
     [SerializeField] float speed;
     [SerializeField] float defaultFOV;
+    [SerializeField] float desiredFOV;
+    [SerializeField]
+    [Range(0f, 5f)] float smoothTime;
     private Controller RR;
     private void Awake()
     {
@@ -19,12 +22,31 @@ public class CameraController : MonoBehaviour
     private void FixedUpdate()
     {
         follow();
+        BoostFOV();
     }
     private void follow()
     {
-        speed = Mathf.Lerp(speed, RR.KPH / 5, Time.deltaTime);
+        if (speed < 25)
+        {
+            speed = Mathf.Lerp(speed, RR.KPH / 5, Time.deltaTime);
+        }
+        else
+        {
+            speed = 25;
+        }
+        
         gameObject.transform.position = Vector3.Lerp(transform.position, child.transform.position, Time.deltaTime * speed);
         gameObject.transform.LookAt(player.gameObject.transform.position);
-
+    }
+    void BoostFOV()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, desiredFOV, Time.deltaTime * smoothTime);
+        }
+        else
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, defaultFOV, Time.deltaTime * smoothTime);
+        }
     }
 }
