da6e2c26bd3a9d2abd48f43e7a2f283a27ee89d1
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-10-06 22:37:58 +0500

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,20 +7,22 @@ public class CameraController : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] GameObject child;
     [SerializeField] float speed;
+    [SerializeField] float defaultFOV;
     private Controller RR;
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         child = player.transform.Find("CameraConstraint").gameObject;
         RR = player.GetComponent<Controller>();
+        defaultFOV = Camera.main.fieldOfView;
     }
     private void FixedUpdate()
     {
         follow();
-        speed = (RR.KPH > 50) ? 20 : RR.KPH / 4;
     }
     private void follow()
     {
+        speed = Mathf.Lerp(speed, RR.KPH / 5, Time.deltaTime);
         gameObject.transform.position = Vector3.Lerp(transform.position, child.transform.position, Time.deltaTime * speed);
         gameObject.transform.LookAt(player.gameObject.transform.position);
 
