b6d6709fd446cf2f8ae7ef3d137a696a4eff05e3
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-10-08 23:58:58 +0500

    Create GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Controller RR;
+    [SerializeField] Text speedo;
+    [SerializeField] float vehicleSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedo.text = "0";
+    }
+
+    private void FixedUpdate()
+    {
+        vehicleSpeed = RR.KPH;
+        UpdateSpeed();
+    }
+    void UpdateSpeed()
+    {
+        speedo.text = vehicleSpeed.ToString();
+    }
+}
